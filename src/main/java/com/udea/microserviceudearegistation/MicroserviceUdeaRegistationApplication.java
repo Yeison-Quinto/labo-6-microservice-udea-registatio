212fc70aae2363cfe51cab97af3090607b75dd3c
Yeison D. Quinto <yeison.quinto@udea.edu.co>
2018-10-29 23:08:16 -0500

    microservice-udea-registatio

---
+++ src/main/java/com/udea/microserviceudearegistation/MicroserviceUdeaRegistationApplication.java
@@ -0,0 +1,12 @@
+package com.udea.microserviceudearegistation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUdeaRegistationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUdeaRegistationApplication.class, args);
+	}
+}
